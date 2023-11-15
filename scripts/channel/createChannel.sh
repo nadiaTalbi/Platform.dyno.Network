ec316c452730d8cf5b32942135352fcaa177b8f5
nadia800 <147506022+nadiaTalbi@users.noreply.github.com>
2023-11-15 16:10:55 +0100

    Update Peers 2

--- scripts/channel/createChannel.sh
+++ scripts/channel/createChannel.sh
@@ -3,7 +3,7 @@ ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrga
 CORE_PEER_TLS_ENABLED=true
 
 echo "Creating channel..."
-peer channel create -o orderer.example.com:7050 /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -c dynochannel --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -f ./channel-artifacts/channel.tx
+peer channel create -o orderer.example.com:7050 /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -c mychannel --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -f ./channel-artifacts/channel.tx
 #peer channel create: This is the command to create a new channe
 #-o orderer.example.com:7050: Specifies the orderer's address and port.
 #--tls $CORE_PEER_TLS_ENABLED: Indicates whether TLS should be used
@@ -14,6 +14,6 @@ peer channel create -o orderer.example.com:7050 /opt/gopath/src/github.com/hyper
 
 echo 
 echo "Channel created, joining dyno..."
-peer channel join -b dynochannel.block
+peer channel join -b genesis.block
 #peer channel join: This is the command to make a peer join a channel
 #-b dynochannel.block: Specifies the path to the latest channel configuration block file 
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -10,8 +10,8 @@ echo "Generating cryto material for peers..."
 
 echo "Generating channel artifacts and genesis block..."
 
-#This command uses the configtxgen tool to generate the genesis block for the orderer based on the specified profile (TwoOrgsOrdererGenesis). The generated block is placed in the channel-artifacts directory
+#This command uses the configtxgen tool to generate the genesis block for the orderer based on the specified profile (OrgsOrdererGenesis). The generated block is placed in the channel-artifacts directory
 ../bin/configtxgen -profile OrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
 
-#This command generates the channel configuration transaction for the specified channel ID (dynochannel) using the profile TwoOrgsChannel. The resulting transaction is saved as channel.tx in the channel-artifacts directory
-../bin/configtxgen -profile OrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID dynochannel
+#This command generates the channel configuration transaction for the specified channel ID (dynochannel) using the profile OrgsChannel. The resulting transaction is saved as channel.tx in the channel-artifacts directory
+../bin/configtxgen -profile OrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
