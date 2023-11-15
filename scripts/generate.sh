febd17c409def45977af43f2df70a7843b165cfb
nadia800 <147506022+nadiaTalbi@users.noreply.github.com>
2023-11-15 16:44:19 +0100

    Update generate.sh

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -11,7 +11,7 @@ echo "Generating cryto material for peers..."
 echo "Generating channel artifacts and genesis block..."
 
 #This command uses the configtxgen tool to generate the genesis block for the orderer based on the specified profile (OrgsOrdererGenesis). The generated block is placed in the channel-artifacts directory
-../bin/configtxgen -profile OrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+../bin/configtxgen -profile OrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block -channelID mychannel
 
 #This command generates the channel configuration transaction for the specified channel ID (dynochannel) using the profile OrgsChannel. The resulting transaction is saved as channel.tx in the channel-artifacts directory
 ../bin/configtxgen -profile OrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
