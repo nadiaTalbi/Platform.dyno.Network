72d954bd90ad2e9034b6bc3b49532f421ff9c0b1
nadia800 <147506022+nadiaTalbi@users.noreply.github.com>
2023-11-16 10:18:50 +0100

    asset transfer

---
+++ asset-transfer/asset.ts
@@ -0,0 +1,15 @@
+/*
+  SPDX-License-Identifier: Apache-2.0
+*/
+
+import {Object} from 'fabric-contract-api';
+
+Object
+export class Asset {
+    
+    public PrivateKey: string;
+
+    public Balance: number;
+
+    public PublicAddress: string;
+}
