55cbf36ca1840229dff03489edc812db4c56101c
nadia800 <147506022+nadiaTalbi@users.noreply.github.com>
2023-11-16 12:21:15 +0100

    Update chaincode

---
+++ asset-transfer/src/asset.ts
@@ -0,0 +1,11 @@
+/*
+  SPDX-License-Identifier: Apache-2.0
+*/
+export class Asset {
+
+    public PrivateKey!: string;
+
+    public Balance!: number;
+
+    public PublicAddress!: string;
+}
---
+++ asset-transfer/src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {ChaincodeContract} from './chaincode-typescript';
+
+export {ChaincodeContract} from './chaincode-typescript';
+
+export const contracts = [ChaincodeContract];
